6ac82464c3a4b547483743844b15987766279bd6
Jan Redmann <janredmann1@gmail.com>
2022-04-19 15:28:01 +0200

    cleanup

--- node-basic-auth/hash.js
+++ node-basic-auth/hash.js
@@ -1,9 +1,1 @@
 const bcrypt = require('bcryptjs')
-
-const password = '156'
-
-const salt = '$2a$10$WcfXc8sGaMrFk6uCWjXJLu'
-console.log(salt)
-console.log(salt.length)
-const hash = bcrypt.hashSync(password, salt)
-console.log(hash)
